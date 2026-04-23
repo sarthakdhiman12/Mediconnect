93a05e1ad2d54a32b241820c6d9445921b405eb2
sarthakdhiman12 <sarthakdhiman261@gmail.com>
2026-04-23 06:03:04 +0000

    day16

--- client/src/app/mediconnect/models/DoctorDTO.ts
+++ client/src/app/mediconnect/models/DoctorDTO.ts
@@ -1,4 +1,42 @@
 
-export class DoctorDTO {
+// export class DoctorDTO {
 
+// }
+
+export class DoctorDTO {
+  doctorId: number;
+  username: string;
+  password: string;
+  fullName: string;
+  contactNumber: string;
+  email: string;
+  specialty: string;
+  yearsOfExperience: number;
+
+  constructor(
+    doctorId: number,
+    username: string,
+    password: string,
+    fullName: string,
+    contactNumber: string,
+    email: string,
+    specialty: string,
+    yearsOfExperience: number
+  ) {
+    this.doctorId = doctorId;
+    this.username = username;
+    this.password = password;
+    this.fullName = fullName;
+    this.contactNumber = contactNumber;
+    this.email = email;
+    this.specialty = specialty;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  logAttributes(): void {
+    Object.entries(this).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+    });
+  }
 }
+``
