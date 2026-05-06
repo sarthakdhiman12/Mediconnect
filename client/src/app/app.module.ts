444aa064ac490fbeeede88172adfbef6a779a1d5
sarthakdhiman12 <sarthakdhiman261@gmail.com>
2026-05-06 13:57:29 +0000

    day25

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'mediconnect',
-    loadChildren: () =>
-      import('./mediconnect/mediconnect.module').then(m => m.MediconnectModule)
-  },
-  {
-    path: '',
-    redirectTo: 'auth',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: 'auth'
-  }
-];
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';  
+import { AuthInterceptor } from './auth.interceptors';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule 
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule {}
+export class AppModule {}
